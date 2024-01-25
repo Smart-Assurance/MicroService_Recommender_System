a36aa3b018eb05e543561a60318f22f4f52cbc5e
elouahabihind <hind20028@gmail.com>
2024-01-25 20:36:42 +0100

    format of response

--- src/kafka/recomendation.py
+++ src/kafka/recomendation.py
@@ -6,13 +6,14 @@ from kafka import KafkaProducer
 
 
 FEATURES_KAFKA_TOPIC = "Features"
-RECOMENDATION_KAFKA_TOPIC = "recomendation"
+RECOMMANDATION_KAFKA_TOPIC = "recommandation"
 
 consumer = KafkaConsumer(
     FEATURES_KAFKA_TOPIC, 
-    bootstrap_servers="localhost:29092"
+    bootstrap_servers="localhost:9092"
 )
-producer = KafkaProducer(bootstrap_servers="localhost:29092")
+
+producer = KafkaProducer(bootstrap_servers="localhost:9092")
 
 
 print("Gonna start listening")
@@ -21,6 +22,19 @@ while True:
         print("Ongoing recomendation..")
         consumed_message = json.loads(message.value.decode())
         print(consumed_message)
+        # featching
+        #   
+        #
+        # producer.send(RECOMENDATION_KAFKA_TOPIC, json.dumps(consumed_message).encode("utf-8"))
+        
+        # Your data
+        data = {
+            "id_contract": "456",
+            "id_client": "65b242ee78bbaf1d76b76104"
+        }
 
-        producer.send(RECOMENDATION_KAFKA_TOPIC, json.dumps(consumed_message).encode("utf-8"))
+        # Convert the data to a JSON string
+        json_data = json.dumps(data)
 
+        # Send the JSON string to the Kafka topic
+        producer.send(RECOMMANDATION_KAFKA_TOPIC, json_data.encode('utf-8'))
